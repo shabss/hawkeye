dc766b5ae2d64194a87542353e9c8ad139990c24
Shabbir Suterwala <shabbirsuterwala@gmail.com>
2016-01-22 19:09:17 +0000

    Updated batch submit script; set PYTHONPATH properly

--- pipeline/hawkeye-spark-submit.sh
+++ pipeline/hawkeye-spark-submit.sh
@@ -1,4 +1,7 @@
- $SPARK_HOME/bin/spark-submit \
-  --master spark://ip-172-31-2-168:7077 \
-  --deploy-mode client \
-  /home/ubuntu/projects/hawkeye/pipeline/hawkeye-batch.py 
+export PYTHONPATH=$SPARK_HOME/python:$SPARK_HOME/python/build:$SPARK_HOME/python/lib/py4j-0.8.2.1-src.zip:$PYTHONPATH
+/home/ubuntu/projects/hawkeye/pipeline/hawkeye-batch.py
+
+# $SPARK_HOME/bin/spark-submit \
+#  --master spark://ip-172-31-2-168:7077 \
+#  --deploy-mode client \
+#  /home/ubuntu/projects/hawkeye/pipeline/hawkeye-batch.py 
