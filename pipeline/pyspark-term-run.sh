f77fd46e70a3c8add2497dd5f698e2266d279e42
Shabbir Suterwala <shabbirsuterwala@gmail.com>
2016-01-22 16:04:06 +0000

    1) prod.py - added HwType and HWID monitors 2) a) Added batch processing layer b) cassandra schemas c) shell script to run ipython shell

---
+++ pipeline/pyspark-term-run.sh
@@ -0,0 +1,1 @@
+IPYTHON=1 $SPARK_HOME/bin/pyspark --master spark://ip-172-31-2-168:7077 --packages datastax:spark-cassandra-connector:1.5.0-RC1-s_2.11
--- producer/prod.py
+++ producer/prod.py
@@ -6,11 +6,8 @@ import random
 import json
 import time
 from datetime import datetime
-
-#logging.basicConfig(
-#    format='%(asctime)s.%(msecs)s:%(name)s:%(thread)d:%(levelname)s:%(process)d:%(message)s',
-#    level=logging.DEBUG
-#)
+from time import time
+import sys
 
 TOT_SWTYPE 			= 100
 TOT_SWID			= 100
@@ -22,7 +19,9 @@ TOT_HWTYPE			= 100
 TOT_HWID			= 100000
 
 TOT_PACKETID		= 100000
-PACKET_DELAY_MAX	= 100000
+
+MAX_PACKET_DELAY	= 100000
+MAX_MESSAGES		= 1000000
 
 class hawkeyedat:
 	AppID = ""
@@ -51,38 +50,51 @@ class hawkeyedat:
 		
 	def popSW(self):
 		self.SwID = "SWID%s" % random.randint(0, TOT_SWID)
-		self.SwType = "SWTYPE%s" % random.randint(0,TOT_SWTYPE)
+		self.SwType = "SWTYPE%s" % random.randint(0, TOT_SWTYPE)
 		
 	def popTask(self):
-		self.TaskID = "TASKID%s" % random.randint(0,TOT_TASKID)
-		self.TaskType = "TASKTYPE%s" % random.randint(0,TOT_TASKTYPE)
+		self.TaskID = "TASKID%s" % random.randint(0, TOT_TASKID)
+		self.TaskType = "TASKTYPE%s" % random.randint(0, TOT_TASKTYPE)
 
 	def popHW(self):
-		self.HwID = "HWID%s" % random.randint(0,TOT_HWID)
-		self.HwType = "HWTYPE%s" % random.randint(0,TOT_HWTYPE)
+		self.HwID = "HWID%s" % random.randint(0, TOT_HWID)
+		self.HwType = "HWTYPE%s" % random.randint(0, TOT_HWTYPE)
 
 	def popPacket(self):
-		self.PacketID = "PACKET%s" % random.randint(0,TOT_PACKETID)
-		self.TsIn = datetime.now().microsecond
-		self.TsOut = self.TsIn + random.randint(0,PACKET_DELAY_MAX)
+		self.PacketID = "PACKET%s" % random.randint(0, TOT_PACKETID)
+		self.TsIn = self.getNowMicrosec()
+		self.TsOut = self.TsIn + random.randint(0, MAX_PACKET_DELAY)
+		
+	def getNowMicrosec(self):
+		#To Do: Get consistent timestamp across all systems
+		return int (round(time() * 1000000))
+		
 	
 random.seed(3002)
 cluster = kafka.KafkaClient("52.34.46.84:9092")
 prod = kafka.SimpleProducer(cluster, async=False)
 topic = "hawkeye-prod1"
 doProd = True
+msgCount = 0
+minTime = datetime.min
+printOnly = sys.argv[1] == "printonly"
 
-
-while doProd:
+while msgCount < MAX_MESSAGES:
 	hmsg = hawkeyedat()
 	hmsg.popApp()
 	hmsg.popSW()
+	hmsg.popHW()
 	hmsg.popTask()
 	hmsg.popPacket()
 	hmsgjson = json.dumps(hmsg, default=lambda o: o.__dict__)
-	print hmsgjson
-	prod.send_messages(topic, hmsgjson)
-	doProd = False
-
+	#print hmsgjson
+	if printOnly == 0:
+		prod.send_messages(topic, hmsgjson)
+		if msgCount % 10000 == 0:
+			print "Messages sent: %s" % msgCount
+	else:
+		print hmsgjson
+	msgCount = msgCount + 1
+	 
 #msg_list = ["{msg : 'first message'}", "{msg: 'second message'}"]
 #prod.send_messages(topic, *msg_list)
