32da067da8ab8094ae94278d3bece881cf6ce12e
Shabbir Suterwala <shabbirsuterwala@gmail.com>
2016-01-22 18:45:54 +0000

    1) Make batch.py run from cmdline 2) term-run script; increase memory 3) First version of spark submit

--- pipeline/hawkeye-batch.py
+++ pipeline/hawkeye-batch.py
@@ -1,9 +1,18 @@
+#!/usr/bin/python
 from pyspark.sql import SQLContext
 from pyspark import SparkContext
+from pyspark import SparkConf
+
+#set following values in $SPARK_HOME/conf/spark-env.sh
+#	SPARK_EXECUTOR_MEMORY=14G
+#	SPARK_DRIVER_MEMORY=14G
 
 master_ip = "ip-172-31-2-168"
 master_public_dns = "ec2-52-34-46-84.us-west-2.compute.amazonaws.com"
-sc = SparkContext("spark://" + master_ip + ":7077", "hawkeye")
+
+conf = SparkConf().setAppName("hawkeye")
+sc = SparkContext(conf=conf)
+##sc = SparkContext("spark://" + master_ip + ":7077", "hawkeye")
 
 from cassandra.cluster import Cluster
 cluster = Cluster(['52.34.46.84', '52.89.61.14', '52.27.234.47', '52.24.233.165'])
@@ -28,18 +37,6 @@ def aggTSDelta(heRDD, aggBy):
 	agg = heRDD.map(lambda m: (m.__getattr__(aggBy), (m.TsOut - m.TsIn, 1))).reduceByKey(lambda a,b: (a[0] + b[0], a[1] + b[1]))
 	return agg
 	
-def aggToCassandra(agg):
-	casSession = cluster.connect('hawkeye3')
-	casSession.execute("""
-		INSERT INTO sliding_window_batch 
-			(monitor, ts_start, time_total)
-		VALUES
-			(%s, %s, %s)
-	""",
-		(agg[0], 0, agg[1])
-	)
-	casSession.shutdown()
-	
 def aggToCassandraPart(agg):
 	from cassandra.cluster import Cluster
 	if agg:
@@ -51,28 +48,23 @@ def aggToCassandraPart(agg):
 		cascluster.shutdown()
 		
 
-def aggToMonitor(agg):
-	return (agg[0], 0, agg[1])
-
-def aggToMonitorPart(agg):
-	aggList = []
-	if agg:
-		for mon, ts in enumerate(agg):
-			aggList.append((mon,ts))
-	return aggList
-	
-
 aggAppID	= aggTSDelta(hemsgs, "AppID")
-aggPacketID = aggTSDelta(hemsgs, "PacketID")
 aggSwID 	= aggTSDelta(hemsgs, "SwID")
 aggSwType 	= aggTSDelta(hemsgs, "SwType")
 aggTaskID	= aggTSDelta(hemsgs, "TaskID")
 aggTaskType	= aggTSDelta(hemsgs, "TaskType")
 #aggHwID 	= aggTSDelta(hemsgs, "HwID")
 #aggHwType 	= aggTSDelta(hemsgs, "HwType")
+
+#aggPacketID = aggTSDelta(hemsgs, "PacketID")
 	 
-rdd = aggAppID.union(aggPacketID).union(aggSwID).union(aggSwType).union(aggTaskID).union(aggTaskType)#.union(aggHwID).union(aggHwType)
+rdd = aggAppID.union(aggSwID)
+		.union(aggSwType)
+		.union(aggTaskID)
+		.union(aggTaskType)
+		#.union(aggHwID)
+		#.union(aggHwType)
 rdd.foreachPartition(aggToCassandraPart)
 
-#aggAppID.count() + aggPacketID.count() + aggSwID.count() + aggSwType.count() + aggTaskID.count() + aggTaskType.count() - rdd.count()
+#sanity = aggAppID.count() + aggPacketID.count() + aggSwID.count() + aggSwType.count() + aggTaskID.count() + aggTaskType.count() - rdd.count()
 
--- pipeline/hawkeye-spark-submit.sh
+++ pipeline/hawkeye-spark-submit.sh
@@ -1,8 +1,4 @@
  $SPARK_HOME/bin/spark-submit \
-  --class <main-class>
   --master spark://ip-172-31-2-168:7077 \
-  --deploy-mode <deploy-mode> \
-  --conf <key>=<value> \
-  ... # other options
-  <application-jar> \
-  [application-arguments]
+  --deploy-mode client \
+  /home/ubuntu/projects/hawkeye/pipeline/hawkeye-batch.py 
